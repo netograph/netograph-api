e234e67f1dcc60d0c0c0c0a330803fb14328dff8
kajojify <ttahabatt@gmail.com>
2020-12-08 11:07:36 +0200

    Excess example code

--- go/cmd/ngc/cli/api/capture.go
+++ go/cmd/ngc/cli/api/capture.go
@@ -12,23 +12,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
 func captureCommand() *cobra.Command {
 	var notification *string
 	var meta *[]string
